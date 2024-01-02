11422e7fd0abe0ab5a9cb1c7ff79ae5ef2291919
IgpI <65492875+IgpI@users.noreply.github.com>
2024-01-02 21:46:46 +0900

    Create basic_Syntax_bash.sh

---
+++ basic_Syntax_bash.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set LS = 'ls -l'
+
+echo "Hello! $(whoami)!"
+echo "You are in $(hostname) now ($(date)) "
+
+echo "Please input file name or directory name that you wanna find"
+read search_name
+
+
+#ファイルを検索
+#search file
+if [ -f "./$search_name" ]
+  then echo "Yes,there is $search_name"
+  else echo "NO,there is not $search_name"
+fi
+
+#ディレクトリを検索
+#search directory
+if [ -d "./$search_name" ]
+  then echo "Yes,there is $search_name"
+  else echo "NO,there is not $search_name"
+fi
+
+
+while read line
+do
+ ls -l $line
+done < input.txt
